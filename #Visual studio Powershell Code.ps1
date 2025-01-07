d6eac3dd2527f5d887d9ef31f55800ffd2d053fb
Wabitz <v.assentoft@gmail.com>
2025-01-07 12:36:01 +0100

    Version Alpha 0.0

---
+++ #Visual studio Powershell Code.ps1
@@ -0,0 +1,69 @@
+#-------------------------------------------------------------------------------------------------------------------------------
+
+#Visual studio Powershell Code for automation of AD user add
+
+#Variable List:
+ # The AD Group the script the users will be added to!
+ # In our case we have a group called MonkeyMembers & our Domain is @Monkey.local
+ $adGroup = "MonkeyMembers"
+ $DomainPrefix = "@Monkey.local"
+ 
+# Ask the script-runner about user details (First name)
+$firstName = Read-Host "Enter the user's first name"
+# Ask the script-runner about user details (Middle name)
+$middleName = Read-Host "Enter the user's middle name (optional)"
+# Ask the script-runner about user details (Last name)
+$lastName = Read-Host "Enter the user's last name"
+
+# Assemble the FullName Variable! (and if your name doesn't have to contain a middlename we have accounted for that! :o )
+if ($middleName) {
+    $fullName = "$firstName $middleName $lastName"
+} else {
+    $fullName = "$firstName $lastName"
+}
+
+# Generate the username (e.g., FirstName.LastName)
+#Takes your first name and lastname and makes it all lowercase with a dot to sepperate the two :)
+$username = "$firstName.$lastName".ToLower()
+
+# Generate the default password (e.g., FirstNameLastInitial@2025)
+# we have decided to do the password, Name(first initial of your last name)@Currentyear(this being 2025 in this case)
+$currentYear = (Get-Date).Year
+$passwordPlain = "$firstName$($lastName.Substring(0,1))@$currentYear"
+$password = ConvertTo-SecureString $passwordPlain -AsPlainText -Force
+
+# Confirm the full name/username/temporaryPass (we ask the Script-Runner to confirm since if there's a typo it's not easy to correct ESPECIALLY WHEN WORKING WITH AZURE!)
+# Also i'm in love with confirmation pages... and reading out what info i've inputted along the way.
+Write-Host "Full Name: $fullName"
+Write-Host "Username: $username"
+Write-Host "Default Password: $passwordPlain"
+$confirmation = Read-Host "Is this information correct? Password is Temporary and will be changed upon logon! (yes/no)"
+
+# If yes the script will try and add the user to the AD Group (in our case MonkeyMembers) No/Nothing/Typo we have an else statement that calls you stupid XD
+if ($confirmation -eq "yes") {
+    Write-Host "Creating user account for $fullName..."
+
+    # Using the Get-ADUser combined with SamAccountName to find any username Equal to our Username.
+    $existingUser = Get-ADUser -Filter { SamAccountName -eq $username }
+    if ($existingUser) {
+        Write-Host "Error!!! A user already has the username '$username' please run the script again and try something else."
+    } else {
+        # This creates the ADuser
+        New-ADUser -Name $fullName `
+            -GivenName $firstName `
+            -Surname $lastName `
+            -SamAccountName $username `
+            -UserPrincipalName "$username$DomainPrefix" `
+            -AccountPassword $password `
+            -Enabled $true
+
+        # After user creation we will add it to the group in our case MonkeyMembers!
+        Add-ADGroupMember -Identity $adGroup -Members $username
+        Write-Host "Success! :o $fullName ($username) has been added to the group '$adGroup'."
+        Write-Host "Temp/Default Password: $passwordPlain (ask the user to change it at first login)."
+    }
+} else {
+    Write-Host "Script Failed! mayday mayday. Are you stupid? why didn't you type Yes >:C"
+}
+
+#-------------------------------------------------------------------------------------------------------------------------------
---
+++ Automatic User Creation Via Documents.ps1
@@ -0,0 +1,88 @@
+#-------------------------------------------------------------------------------------------------------------------------------
+
+#Automatic User Creation Via Documents
+
+#Stupid function that does NATHING... NATHING (it finds the CSV folders information and pastes it out.)
+Function PathInformationFunction {
+    Param (
+        #Variable List:
+        #Variable for the CSV Path, IMPORTANT!
+        $CSV_PATH = "C:\KodningFolder\User creation folder.csv")
+        #First we start by importing the code.
+        $CSV_COMPLETION = Import-CSV -Path "$CSV_PATH"
+
+        #Were making an if statement to see if the CSV path was valid :)
+        if ($CSV_COMPLETION) {
+            Write-Host "The CSV PATH: '$CSV_PATH' is functional"
+            # Her bruger vi Foreach jeg fandt på microsoft learn.
+            # Vi skal bruge dette da vi skal havde en function kørende der lopper indtil dokumentet er tomt.
+            foreach ($row in $CSV_COMPLETION) {
+                    $UserID = $row.USERID
+                    $FirstName = $row.FIRSTNAME
+                    $MiddleName = $row.MIDDLENAME
+                    $LastName = $row.LASTNAME
+
+                    Write-Host "UserID: $UserID, First Name: $FirstName, Middle Name: $MiddleName, Last Name: $LastName"}
+        } else {
+    Write-Host "did not work"}
+}
+
+#Function that will one by one create users in the AD group.
+Function UserCreationFunction {
+    Param (
+        #Variable List:
+        # The AD Group the script the users will be added to!
+        # In our case we have a group called MonkeyMembers & our Domain is @Monkey.local
+        $adGroup = "MonkeyMembers",
+        $DomainPrefix = "@Monkey.local",
+
+        #Variables for the user creation.
+        $UserID,
+        $Firstname,
+        $Middlename,
+        $Lastname
+        )
+            Write-Host "Creating user account for $fullName..."
+        
+            # Using the Get-ADUser combined with SamAccountName to find any username Equal to our Username.
+            $existingUser = Get-ADUser -Filter { SamAccountName -eq $username }
+            if ($existingUser) {
+                Write-Host "Error!!! A user already has the username '$username' please run the script again and try something else."
+            } else {
+                # This creates the ADuser
+                New-ADUser -Name $fullName `
+                    -GivenName $firstName `
+                    -Surname $lastName `
+                    -SamAccountName $username `
+                    -UserPrincipalName "$username$DomainPrefix" `
+                    -AccountPassword $password `
+                    -Enabled $true
+        
+                # After user creation we will add it to the group in our case MonkeyMembers!
+                Add-ADGroupMember -Identity $adGroup -Members $username
+                Write-Host "Success! :o $fullName ($username) has been added to the group '$adGroup'."
+                Write-Host "Temp/Default Password: $passwordPlain (ask the user to change it at first login)."
+            }
+        } else {
+            Write-Host "UserCreation Script Failed! mayday mayday. Are you stupid? why didn't you type Yes >:C"
+}
+
+#This is the start of the script! (excluding functions of course)
+
+#We are calling the function that reads out the CSV file via PathInformationFunction
+    PathInformationFunction
+
+    #We are adding a small pop up that warns the user that by pressing enter they agree to the following users stated will be added to the viable adgroup.
+    Read-Host "`nIs this information correct? (Press 'ENTER' to continue) `nWARNING!!! Pressing Enter will begin the Process of adding every user to the '$adGroup'"
+    foreach ($row in $CSV_COMPLETION) {
+        $UserID = $row.USERID
+        $FirstName = $row.FIRSTNAME
+        $MiddleName = $row.MIDDLENAME
+        $LastName = $row.LASTNAME
+
+        UserCreationFunction($UserID, $FirstName, $MiddleName, $LastName)
+    } else {
+Write-Host "$UserID did not work"}
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
