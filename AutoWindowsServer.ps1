07d3043f586ba66160483e8f923de185856bcc59
Lucky6060 <joachim.wich@gmail.com>
2025-01-07 10:01:26 +0100

    installs ADDS, DNS and WIN on windows server 2022 core

---
+++ AutoWindowsServer.ps1
@@ -0,0 +1,44 @@
+
+# Variables
+$NewComputerName = Read-Host "Enter the desired server name"                        # New computer name
+$StaticIP = Read-Host "Enter the desired IP Address"                                # Static IP address
+$SubnetMask = Read-Host "Enter Subnet Mask as number fx. 24 and not 255.255..."     # Subnet mask
+$DefaultGateway = Read-Host "Enter the desired Default Gateway,"                    # Default gateway
+$DNSServer = Read-Host "Enter the desired DNS Server"                               # Primary DNS server
+$DomainName = Read-Host "Enter the desired Domain Name, like yourdomain.com"        # Desired domain name
+$NetBIOSName = Read-Host "Enter the desired NetBIOS name, like yourdomain"          # NetBIOS name
+$SafeModePassword = Read-Host "Enter the desired SafeModePassword"                  # Secure password for DSRM
+
+#Convert Safe Mode password to a secure string
+$SecureSafeModePassword = ConvertTo-SecureString $SafeModePassword -AsPlainText -Force
+
+#Set Static IP Address
+$Interface = Get-NetAdapter | Where-Object {$_.Status -eq "Up"} #Get the active network interface
+if ($Interface) {
+    New-NetIPAddress -InterfaceIndex $Interface.InterfaceIndex `
+        -IPAddress $StaticIP `
+        -PrefixLength $SubnetMask `
+        -DefaultGateway $DefaultGateway
+    Set-DnsClientServerAddress -InterfaceIndex $Interface.InterfaceIndex -ServerAddresses $DNSServer
+    Write-Host "Static IP address configured: $StaticIP" -ForegroundColor Green
+} else {
+    Write-Host "No active network interface found!" -ForegroundColor Red
+    exit
+}
+
+#Rename the Computer
+Rename-Computer -NewName $NewComputerName -Force
+
+#Install DNS, ADDS, and WIM
+Install-WindowsFeature -Name AD-Domain-Services, DNS, Windows-Internal-Database -IncludeManagementTools
+
+#Promote the Server to a Domain Controller
+Import-Module ADDSDeployment
+
+Install-ADDSForest -DomainName $DomainName `
+    -DomainNetBIOSName $NetBIOSName `
+    -SafeModeAdministratorPassword $SecureSafeModePassword `
+    -Force
+#Post-installation reboot
+Write-Host "Installation complete. Rebooting the server..." -ForegroundColor Green
+Restart-Computer -Force
