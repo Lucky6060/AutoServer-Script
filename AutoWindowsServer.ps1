8f36746dba2f61ebad60cc6ec13b9a6766696343
Lucky6060 <joachim.wich@gmail.com>
2025-01-08 10:14:04 +0100

    Version 0.6

--- AutoWindowsServer.ps1
+++ AutoWindowsServer.ps1
@@ -10,43 +10,48 @@ $SafeModePassword = Read-Host "Enter the desired SafeModePassword"              
 $Interface = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}                     # Get the active network interface
 
 
-#Convert Safe Mode password to a secure string
+# Convert Safe Mode password to a secure string
 $SecureSafeModePassword = ConvertTo-SecureString $SafeModePassword -AsPlainText -Force
 
-#Installs chocolatey on to the server
-Set-ExecutionPolicy Bypass -Scope Process -Force
-C:\Users\Adminstrator\AutoServer-Script\ChocoInstaller\Install.ps1 -ChocolateyDownloadUrl C:\Users\Adminstrator\AutoServer-Script\ChocoInstaller\Chocolatey.nupkg
-
-
-#installs python via chocolatey
+# Installs python via chocolatey
+Write-Host "Installing Python via chocolatey" -ForegroundColor Green
 choco install python -y
 
 
-
-
-#Set Static IP Address
+# Set Static IP Address
 if  ($StaticIP -in @("DHCP", "dhcp")) {
-    Write-Host "IP address is set to DHCP"
+    Write-Host "Configuring IP address as DHCP..." -ForegroundColor Yellow
     if ($Interface) {
-        Set-DhcpClient -InterfaceIndex $Interface.InterfaceIndex
-        Write-Host "DHCP has been enabled for the interface: $($Interface.Name)" -ForegroundColor Green
+        try {
+            Set-DhcpClient -InterfaceIndex $Interface.InterfaceIndex
+            Write-Host "DHCP has been enabled for the interface: $($Interface.Name)" -ForegroundColor Green
+        } catch{
+            Write-Host "Failed to enable DHCP. Error: $_" -ForegroundColor Red
+        }
     }
-} elseif ($Interface) { 
-    New-NetIPAddress -InterfaceIndex $Interface.InterfaceIndex `
-        -IPAddress $StaticIP `
-        -PrefixLength $SubnetMask `
-        -DefaultGateway $DefaultGateway
+} 
+elseif ($Interface) { 
+    Write-Host "Configuring static IP address..." -ForegroundColor Yellow
+    try{
+        New-NetIPAddress -InterfaceIndex $Interface.InterfaceIndex `
+            -IPAddress $StaticIP `
+            -PrefixLength $SubnetMask `
+            -DefaultGateway $DefaultGateway
         Set-DnsClientServerAddress -InterfaceIndex $Interface.InterfaceIndex -ServerAddresses $DNSServer
         Write-Host "Static IP address configured: $StaticIP" -ForegroundColor Green
-    } else {
-        Write-Host "No active network interface found!" -ForegroundColor Red
-        exit
+    } catch{
+        Write-Host "Failed to configure static IP. Error: $_" -ForegroundColor Red
+    }
+ } else {
+     Write-Host "No active network interface found!" -ForegroundColor Red
+    exit
 }
 
-#Install DNS, ADDS, and WIM
+
+# Install DNS, ADDS, and WIM
 Install-WindowsFeature -Name AD-Domain-Services, DNS, Windows-Internal-Database -IncludeManagementTools
 
-#Promote the Server to a Domain Controller
+# Promote the Server to a Domain Controller
 Import-Module ADDSDeployment
 
 Install-ADDSForest -DomainName $DomainName `
@@ -54,8 +59,8 @@ Install-ADDSForest -DomainName $DomainName `
     -SafeModeAdministratorPassword $SecureSafeModePassword `
     -Force
 
-#Rename the Computer
+# Rename the Computer
 Rename-Computer -NewName $NewComputerName -Force
 
-#Post-installation reboot
+# Post-installation reboot
 Write-Host "Installation complete. Reboot the server with Restart-Computer" -ForegroundColor Green
